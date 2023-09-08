ebbc96eff263070e2020d81b4a27f6103cd9f054
ricguer <rj.guerra@outlook.com>
2023-09-08 16:47:48 -0400

    Install and Implement Source Sans Pro as Default Application Font

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+import "@fontsource/source-sans-pro";
+
+
+const theme = createTheme({
+    typography: {
+        fontFamily: "Source Sans Pro, sans-serif"
+    }
+});
+
+export default theme;

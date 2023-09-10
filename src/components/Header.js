4220149f74c51eafb348a4c85fe5628438c4351f
ricguer <rj.guerra@outlook.com>
2023-09-10 12:25:24 -0400

    Temporarily Disable the Navigation Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,13 @@ import React from "react";
 import Typography from '@mui/material/Typography';
 import Navigation from "./Navigation";
 
-function Header() {
+export default function Header() {
     return (
-        <header className="App-header">
-            <Navigation />
+        <header>
+            {/* <Navigation /> */}
             <Typography variant="h1">Ricardo Guerra</Typography>
             <Typography variant="h2">Software Engineer</Typography>
             <Typography variant="h4">Embedded Systems | Desktop Applications | Full-Stack Web Applications</Typography>
         </header>
     );
-}
-
-export default Header;
+};

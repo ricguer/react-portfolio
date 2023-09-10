c9d27bfee210df7eeed69d07725a5d10fbc53efc
ricguer <rj.guerra@outlook.com>
2023-09-10 12:26:13 -0400

    Apply Paper Components to Make All App Components Responsive to Theme Changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import SkillsSection from "./SkillsSection";
 import ProjectsSection from "./ProjectsSection";
 import Footer from "./components/Footer";
 import Particles from './components/Particles.js';
+import { Paper } from '@mui/material';
 
 function App() {
   return (
     <main className="App">
-      <Particles />
-      <Header />
-      <SkillsSection />
-      <ProjectsSection />
-      <Footer />
+      <Paper elevation={0} style={{ position: "absolute", zIndex: "-1", height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Particles />
+        <Paper elevation={4} style={{ position: "absolute", zIndex: "-1", height: "100vh", width: "90vw" }}>
+          <Header />
+          <SkillsSection />
+          <ProjectsSection />
+          {/* <Footer /> */}
+        </Paper>
+      </Paper>
     </main>
-
   );
 }
 

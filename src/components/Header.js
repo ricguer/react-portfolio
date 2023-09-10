52d70aa24ab02775298776b94dce4a259297591f
ricguer <rj.guerra@outlook.com>
2023-09-10 15:31:43 -0400

    Create Sticky Navigation Bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Navigation from "./Navigation";
 export default function Header() {
     return (
         <header>
-            {/* <Navigation /> */}
+            <Navigation />
             <Typography variant="h1">Ricardo Guerra</Typography>
             <Typography variant="h2">Software Engineer</Typography>
             <Typography variant="h4">Embedded Systems | Desktop Applications | Full-Stack Web Applications</Typography>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,15 @@
 import AppBar from "@mui/material/AppBar";
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import { Box, Typography } from "@mui/material";
 
 export default function Navigation() {
     return (
-        <AppBar position="fixed">
-            <Toolbar>
+        <AppBar position="sticky">
+            <Toolbar style={{ justifyContent: "center" }}>
                 <Button color="inherit">About Me</Button>
-                <Button color="inherit" href="#skills">Skills</Button>
-                <Button color="inherit" href="#portfolio">Portfolio</Button>
+                <Button color="inherit">Skills</Button>
+                <Button color="inherit">Portfolio</Button>
             </Toolbar>
         </AppBar>
     );

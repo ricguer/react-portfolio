0b4b8873e9e04d8969a49a8c5699eb15aae14091
ricguer <rj.guerra@outlook.com>
2023-09-10 09:45:56 -0400

    Implement Particles Background

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Header from "./components/Header";
 import SkillsSection from "./SkillsSection";
 import ProjectsSection from "./ProjectsSection";
 import Footer from "./components/Footer";
+import Particles from './components/Particles.js';
 
 function App() {
   return (
     <main className="App">
+      <Particles />
       <Header />
       <SkillsSection />
       <ProjectsSection />

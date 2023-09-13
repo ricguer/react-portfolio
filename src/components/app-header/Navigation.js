053db9737ba7a978f678b357adad1706271184a3
ricguer <rj.guerra@outlook.com>
2023-09-13 18:42:55 -0400

    Add "Resume" and "Contact" Sections in Navigation Bar

--- src/components/app-header/Navigation.js
+++ src/components/app-header/Navigation.js
@@ -2,14 +2,17 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import ThemeToggle from "./ThemeToggle.js";
+import React, { useRef } from "react";
 
 export default function Navigation({ colorModeContext }) {
+
     return (
         <AppBar position="sticky">
             <Toolbar style={{ position: "relative", justifyContent: "center" }}>
                 <Button color="inherit">About Me</Button>
-                <Button color="inherit">Skills</Button>
                 <Button color="inherit">Portfolio</Button>
+                <Button color="inherit">Resume</Button>
+                <Button color="inherit">Contact</Button>
                 <div style={{ position: "absolute", right: 10 }}>
                     <ThemeToggle colorModeContext={colorModeContext} />
                 </div>
